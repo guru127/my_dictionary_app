3d4b361a88b81ac7daca8c56dd81d482f19e2192
Guru127 <gurucharan.gcn@gmail.com>
2021-10-31 00:30:55 +0530

    foooter is added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 import{BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from './components/header/Home';
 import Cards from './components/cards/Cards';
+import Footer from './components/footer/Footer';
 
 function App() {
    return (
     <div className="App" >
        <Container className="conatiner" maxWidth="md">
         <Home/>
-       
+        <Footer/>
        </Container>
     </div>
   );
